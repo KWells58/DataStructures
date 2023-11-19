1a9ed7677412ef662ec36711e130e7668a3cd621
Jesus De La Rosa <117212034+snohoho@users.noreply.github.com>
2023-11-19 14:51:42 -0500

    completed code
    completed code

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+/*
+* File: ds-team-project.zip
+* Input: input1.txt, input2.txt
+* Output: output1.txt, output2.txti ch
+* Team:  
+* Course: COP3415
+* Date: 11/19/2023
+*/
+
 #include <iostream>
 #include <fstream>
 #include <queue>
@@ -7,7 +16,7 @@ using namespace std;
 struct Thread {
     int index; // Index of the thread
     int job_index; // Index of the job the thread is processing
-    int processing_time; // Remaining processing time of the job
+    long long processing_time; // Remaining processing time of the job
 
     // Overloading the less-than operator for the priority queue
     bool operator<(const Thread& other) const {
@@ -16,51 +25,51 @@ struct Thread {
 };
 
 int main() {
-    // Read the input file
-    ifstream inputFile("input1.txt");
-    int n, m;
-    inputFile >> n >> m;
+  // Read the input file
+  ifstream inputFile("input1.txt");
+  int n, m;
+  inputFile >> n >> m;
+  
+  // Create an array of threads
+  Thread threads[n];
+  for (int i = 0; i < n; i++) {
+    threads[i].index = i;
+    threads[i].job_index = -1; // Initialize with an invalid job index
+    threads[i].processing_time = 0; // Initialize with zero processing time
+  }
 
-    // Create an array of threads
-    Thread threads[n];
-    for (int i = 0; i < n; i++) {
-        threads[i].index = i;
-        threads[i].job_index = -1; // Initialize with an invalid job index
-        threads[i].processing_time = 0; // Initialize with zero processing time
-    }
+  // Read the processing times of the jobs
+  long long processingTimes[m];
+  for (int i = 0; i < m; i++) {
+    inputFile >> processingTimes[i];
+  }
 
-    // Read the processing times of the jobs
-    int processingTimes[m];
-    for (int i = 0; i < m; i++) {
-        inputFile >> processingTimes[i];
-    }
+  // Initialize the scheduler
+  priority_queue<Thread> priorityQueue;
+  for (int i = 0; i < n; i++) {
+    priorityQueue.push(threads[i]);
+  }
 
-    // Initialize the scheduler
-    priority_queue<Thread> priorityQueue;
-    for (int i = 0; i < n; i++) {
-        priorityQueue.push(threads[i]);
-    }
+  // Open the output file
+  ofstream outputFile("output1.txt");
 
-    // Open the output file
-    ofstream outputFile("output1.txt");
+  // Simulate the execution of the jobs
+  for (int i = 0; i < m; i++) {
+    // Remove the thread with the highest priority from the priority queue
+    Thread thread = priorityQueue.top();
+    priorityQueue.pop();
 
-    // Simulate the execution of the jobs
-    for (int i = 0; i < m; i++) {
-        // Remove the thread with the highest priority from the priority queue
-        Thread thread = priorityQueue.top();
-        priorityQueue.pop();
+    // Output the result to the console and the file
+    cout << thread.index << " " << thread.processing_time << endl;
+    outputFile << thread.index << " " << thread.processing_time << endl;
+    
+    // Assign the job to the thread
+    thread.job_index = i;
+    thread.processing_time += processingTimes[i];
 
-        // Assign the job to the thread
-        thread.job_index = i;
-        thread.processing_time = processingTimes[i];
-
-        // Output the result to the console and the file
-        cout << thread.index << " " << i << endl;
-        outputFile << thread.index << " " << i << endl;
-
-        // Add the thread back to the priority queue
-        priorityQueue.push(thread);
-    }
+    // Add the thread back to the priority queue
+    priorityQueue.push(thread);
+  }
 
     // Close the output file
     outputFile.close();
